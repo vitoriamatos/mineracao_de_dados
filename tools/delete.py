37522e71a15bf5d8c3d050b8010bc7ad5b1bcbe8
vitoriamatos <vitoria.matos99@gmail.com>
2022-11-23 00:18:05 -0300

    delete ID_MATRICULA column

--- tools/delete.py
+++ tools/delete.py
@@ -2,19 +2,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pydotplus
 
-caminhoBase = "data/database/database_social.csv"
+caminhoBase = "data/database/fit.csv"
 df = pd.read_csv(caminhoBase)
 
-df.drop(columns=['ebairrnome'],  inplace=True)
-df.drop(columns=['anoensino'],  inplace=True)
-df.drop(columns=['esermodesc'],  inplace=True)
-df.drop(columns=['nturesnome'],  inplace=True)
-df.drop(columns=['IN_DEPENDENCIAS_PNE'], inplace=True)
-df.drop(columns=['IN_SECRETARIA'], inplace=True)
-df.drop(columns=['IN_BANHEIRO_CHUVEIRO'], inplace=True)
-df.drop(columns=['IN_REGULAR_x'], inplace=True)
-df.drop(columns=['IN_EJA_x'], inplace=True)
-df.drop(columns=['IN_PROFISSIONALIZANTE_x'], inplace=True)
+# df.drop(columns=['ebairrnome'],  inplace=True)
+# df.drop(columns=['anoensino'],  inplace=True)
+# df.drop(columns=['esermodesc'],  inplace=True)
+# df.drop(columns=['nturesnome'],  inplace=True)
+# df.drop(columns=['IN_DEPENDENCIAS_PNE'], inplace=True)
+# df.drop(columns=['IN_SECRETARIA'], inplace=True)
+# df.drop(columns=['IN_BANHEIRO_CHUVEIRO'], inplace=True)
+# df.drop(columns=['IN_REGULAR_x'], inplace=True)
+# df.drop(columns=['IN_EJA_x'], inplace=True)
+# df.drop(columns=['IN_PROFISSIONALIZANTE_x'], inplace=True)
 df.drop(columns=['id'],  inplace=True)
+df.drop(columns=['ID_MATRICULA'],  inplace=True)
 
-df.to_csv("data/database/fit.csv")
+df.to_csv("data/database/data.csv")
