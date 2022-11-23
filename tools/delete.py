f1fd96be228ce42dd60a544e3dc97faed84debf9
vitoriamatos <vitoria.matos99@gmail.com>
2022-11-23 15:41:31 -0300

    update database

--- tools/delete.py
+++ tools/delete.py
@@ -2,20 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pydotplus
 
-caminhoBase = "data/database/fit.csv"
+caminhoBase = "data/database/evasao-escolar2.csv"
 df = pd.read_csv(caminhoBase)
 
-# df.drop(columns=['ebairrnome'],  inplace=True)
-# df.drop(columns=['anoensino'],  inplace=True)
-# df.drop(columns=['esermodesc'],  inplace=True)
-# df.drop(columns=['nturesnome'],  inplace=True)
-# df.drop(columns=['IN_DEPENDENCIAS_PNE'], inplace=True)
-# df.drop(columns=['IN_SECRETARIA'], inplace=True)
-# df.drop(columns=['IN_BANHEIRO_CHUVEIRO'], inplace=True)
-# df.drop(columns=['IN_REGULAR_x'], inplace=True)
-# df.drop(columns=['IN_EJA_x'], inplace=True)
-# df.drop(columns=['IN_PROFISSIONALIZANTE_x'], inplace=True)
-df.drop(columns=['id'],  inplace=True)
-df.drop(columns=['ID_MATRICULA'],  inplace=True)
 
-df.to_csv("data/database/data.csv")
+
+df.drop(columns=['id'], inplace=True)
+df.drop(columns=['nivelbairro'], inplace=True)
+df.drop(columns=['IN_COZINHA'], inplace=True)
+df.drop(columns=['serie'], inplace=True)
+
+
+
+df.to_csv("data/database/evasao-escolar3.csv")
